78bf2b5efbf9cf69adf2839c9239e1f7d42a32b9
Sven Hepkema <shepkema@sgs-gpu07.ethz.ch>
2025-12-14 19:11:55 +0100

    Created first blas postprocessing prototype kernel

--- include/superkmeans/distance_computers/kernels.cuh
+++ include/superkmeans/distance_computers/kernels.cuh
@@ -1,1 +1,36 @@
-void test();
+#pragma once
+
+#include <cstddef>
+#include <cuda_runtime.h>
+
+#include <Eigen/Eigen/Dense>
+
+#include "superkmeans/common.h"
+// #include "superkmeans/distance_computers/base_computers.h"
+// #include "superkmeans/pdx/layout.h"
+// #include "superkmeans/profiler.h"
+
+
+
+namespace skmeans {
+namespace kernels {
+
+using distance_t = skmeans_distance_t<Quantization::f32>;
+using data_t = skmeans_value_t<Quantization::f32>;
+using norms_t = skmeans_value_t<Quantization::f32>;
+
+void first_blas(
+    const int batch_n_x,
+    const int batch_n_y,
+    const int i,
+    const int j,
+    const norms_t* norms_x,
+    const norms_t* norms_y,
+    float* all_distances_buffer,
+    distance_t* out_distances,
+    uint32_t* out_knn,
+    const cudaStream_t stream
+);
+
+}
+}
