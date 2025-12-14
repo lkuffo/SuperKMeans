7dd48a758d1986052ce1acfb0f74a358d60f4151
Sven Hepkema <shepkema@sgs-gpu07.ethz.ch>
2025-12-15 00:02:59 +0100

    Optimized the first_blas_postprocessing kernel with warp level primitives

--- include/superkmeans/distance_computers/kernels.cu
+++ include/superkmeans/distance_computers/kernels.cu
@@ -22,6 +22,10 @@ inline void check_CUDA_error(cudaError_t code, const char* file, int line, bool 
 
 #define CUDA_SAFE_CALL(ans) check_CUDA_error((ans), __FILE__, __LINE__)
 
+const int32_t WARP_WIDTH = 32;
+const int32_t FULL_MASK = 0xffffffff;
+
+
 template<typename T>
 __device__ T find_min(const T* data, const uint32_t size, const T max, uint32_t* min_index) {
 	T min = max;
@@ -45,6 +49,29 @@ __device__ constexpr T max_of_either(const T a, const T b) {
 	return a;
 }
 
+struct IndexMinWarpReductionResult {
+	int32_t index;
+	float value;
+};
+
+__device__ IndexMinWarpReductionResult index_min_warp_reduction(const int32_t index, const float value) {
+	auto send_index = index;
+	auto send_value = value;
+
+	for (int offset = 16; offset > 0; offset /= 2) {
+    auto receive_index = __shfl_down_sync(FULL_MASK, send_index, offset);
+    auto receive_value = __shfl_down_sync(FULL_MASK, send_value, offset);
+
+		if (receive_value < send_value) {
+			send_value = receive_value;
+			send_index = receive_index;
+		}
+	}
+
+	return IndexMinWarpReductionResult{send_index, send_value};
+
+}
+
 __global__ void first_blas_kernel(
     const int batch_n_x,
     const int batch_n_y,
@@ -57,25 +84,37 @@ __global__ void first_blas_kernel(
     uint32_t* out_knn,
 		const float max
 ) {
-    auto r = blockIdx.x * blockDim.x + threadIdx.x;
+    auto warp_thread_index = threadIdx.x % WARP_WIDTH;
+    auto global_thread_index = blockIdx.x * blockDim.x + threadIdx.x;
+    auto item_index = global_thread_index / WARP_WIDTH;
 
-    if (batch_n_x <= r) {
+    if (batch_n_x <= item_index) {
         return;
     }
 
-    const auto i_idx = i + r;
-    const float norm_x_i = norms_x[i_idx];
-    float* row_p = all_distances_buffer + r * batch_n_y;
+    const float norm_x_i = norms_x[item_index];
+    float* row_p = all_distances_buffer + item_index * batch_n_y;
 
-    for (std::size_t c = 0; c < batch_n_y; ++c) {
-        row_p[c] = -2.0f * row_p[c] + norm_x_i + norms_y[j + c];
+		int32_t knn_idx = 0;
+		float batch_top_1 = max;
+    for (int32_t c = warp_thread_index; c < batch_n_y; c += WARP_WIDTH) {
+        auto result = -2.0f * row_p[c] + norm_x_i + norms_y[c];
+				if (result < batch_top_1) {
+					knn_idx = c;
+					batch_top_1 = result;
+				}
     }
-		uint32_t knn_idx;
-    auto batch_top_1 = find_min(row_p, batch_n_y, max, &knn_idx);
-    if (batch_top_1 < out_distances[i_idx]) {
-        out_distances[i_idx] = max_of_either<float>(0.0f, batch_top_1);
-        out_knn[i_idx] = j + knn_idx;
-    }
+
+		auto result = index_min_warp_reduction(knn_idx, batch_top_1);
+
+		if (warp_thread_index == 0) {
+			knn_idx = result.index;
+			batch_top_1 = result.value;
+			if (batch_top_1 < out_distances[item_index]) {
+					out_distances[item_index] = max_of_either<float>(0.0f, batch_top_1);
+					out_knn[item_index] = j + knn_idx;
+			}
+		}
 }
 
 template<typename T>
@@ -131,10 +170,17 @@ void first_blas(
 	//health_check_buffer(out_knn, i + batch_n_x);
 		const auto max = std::numeric_limits<float>::max();
 
-    const auto N_THREADS = 1024;
-    const auto n_blocks = divide_round_up<int32_t>(batch_n_x, N_THREADS);
-    first_blas_kernel<<<n_blocks, N_THREADS, 0, stream>>>(
-        batch_n_x, batch_n_y, i, j, norms_x, norms_y, all_distances_buffer, out_distances, out_knn, max
+		// ===============
+		// WARNING : It might be the case that batch_n_y % 32 == 0, due to use of warp primitives
+		// NOTE: Other solutions are also possible, but did not test yet
+		// ===============
+
+    const auto N_THREADS_PER_BLOCK = 1024;
+    const auto N_THREADS_PER_ITEM = WARP_WIDTH;
+		const auto ITEMS_PER_BLOCK = divide_round_up<int32_t>(N_THREADS_PER_BLOCK, N_THREADS_PER_ITEM);
+    const auto n_blocks = divide_round_up<int32_t>(batch_n_x, ITEMS_PER_BLOCK);
+    first_blas_kernel<<<n_blocks, N_THREADS_PER_BLOCK, 0, stream>>>(
+        batch_n_x, batch_n_y, i, j, norms_x + i, norms_y + j, all_distances_buffer, out_distances + i, out_knn + i, max
     );
 		//CUDA_SAFE_CALL(cudaDeviceSynchronize());
 }
